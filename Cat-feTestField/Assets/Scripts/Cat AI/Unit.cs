90fec754a8f5ef1aed97a95b061950658e76bbde
Sebward <samueleburgoyne@gmail.com>
2023-11-17 01:12:07 -0500

    Revert "Merge branch 'main' into Playtest2"
    This reverts commit 41ab21a2237f0ee2dceede8b95eaa4e0e13f14c5, reversing
    changes made to 59e04f730e4f4834b94e1deb2422174ef551e38b.

--- Cat-feTestField/Assets/Scripts/Cat AI/Unit.cs
+++ Cat-feTestField/Assets/Scripts/Cat AI/Unit.cs
@@ -7,11 +7,11 @@ namespace RoosaIGM601
 {
     public class Unit : MonoBehaviour
     {
-
+        
         //private Transform target;
         public Transform start;
-        private Vector3 target;
-        float speed = 5f;
+        private Vector3 target; 
+        float speed = 5;
         Vector3[] path;
         int targetIndex;
 
@@ -31,12 +31,8 @@ namespace RoosaIGM601
         Vector3 targetLocation;
         Vector3 previousLocation;
 
-        private float rotSpeed = 10f;
-        bool isRotating = true;
-        private string callFrom;
-        private Vector3 currentWaypoint;
+        private float rotSpeed = 30f;
         //private float targetRadius = 0.5f;
-        
 
         //communicate with leveleditor
         private bool canNavigate = true;
@@ -54,7 +50,7 @@ namespace RoosaIGM601
             catAnimator = catModel.GetComponent<Animator>();
 
             target = start.position;
-            PathManager.RequestPath(start.position, target, OnPathFound);
+            PathManager.RequestPath(start.position,target, OnPathFound);
 
             LevelEditorManager.FurnitureBuilding += ToggleNavigation;
 
@@ -80,27 +76,11 @@ namespace RoosaIGM601
             //Wander();
             ChooseAction();
             // Check for user input to set a new target position
-            if (Input.GetMouseButtonDown(0) && canNavigate)
-            {
-                ClickedLocation();
-            }
-        }
-
-        void FixedUpdate()
-        {
-            Debug.Log(isRotating);
-            if (!isRotating)
-            {
-                if(callFrom == "Wandering"){
-                    Debug.Log("Inside Wander");
-                    MoveTo(targetLocation);
-                }
-                else{
-                    MoveTo(target);
-                    //transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
-                    //transform.LookAt(currentWaypoint);
-                }
-            }
+            // if (Input.GetMouseButtonDown(0) && canNavigate)
+            // {
+            //     ClickedLocation();
+            //     //CatRotation();
+            // }
         }
 
         public void InvestigateObject()
@@ -110,19 +90,19 @@ namespace RoosaIGM601
 
         private void Wander()
         {
-            //Debug.Log("Is wandering");
             if (!completingAction)
             {
+                //Debug.Log("Finding new location");
                 //Find new random location to wander to
                 targetLocation = new Vector3(Random.Range(-4.0f, 4.0f), 0f, Random.Range(-4.0f, 4.0f));
 
                 //Speed of how fast the cat will move to this location
                 speed = Random.Range(1f, 3f);
 
+                //Debug.Log(targetLocation);
+
                 //Move to new location and set completing action to true
                 MoveTo(targetLocation);
-                callFrom = "Wandering";
-                //CatRotation(targetLocation);
                 completingAction = true;
 
                 //Activate Animation
@@ -297,7 +277,7 @@ namespace RoosaIGM601
             //Return back to normal
         }
 
-        private void ChooseAction()
+        public void ChooseAction()
         {
             //Debug.Log("Is idle" + isIdle);
             if (isIdle)
@@ -312,43 +292,51 @@ namespace RoosaIGM601
 
         public void ClickedLocation()
         {
+            //Debug.Log("Roosa");
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+            //Debug.Log("Ray origin: " + ray.origin);
+            //Debug.Log("Ray direction: " + ray.direction);
+
             if (Physics.Raycast(ray, out hit))
             {
+                //Debug.Log("Hello Roosa");
                 target = hit.point;
-                //MoveTo(target);
-                CatRotation(target);
+                //Debug.Log("New target position: " + target);
+                //CatRotation(target);
+                MoveTo(target);
+                
+                //PathManager.RequestPath(start.position, target, OnPathFound); // Recalculate the path to the new target
             }
         }
 
+        public void CatRotation(Vector3 targetPos){
+        // //     // Vector3 dir = target- transform.position;
+        // //     // Quaternion rot = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), rotSpeed * Time.deltaTime);
+        // //     // rot.y = 0;
+        // //     // rot.z = 0;
+        // //     // transform.rotation = rot;
 
-        public void CatRotation(Vector3 targetPos)
-        {
-            Vector3 directionToClick = targetPos - transform.position;
-            directionToClick.y=0;
-            directionToClick.Normalize();
-            Quaternion toRotation = Quaternion.LookRotation(directionToClick, Vector3.up);
-            //transform.LookAt(targetPos);
-            StartCoroutine(RotateAndMove(toRotation));
-        }
+        // //     // float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * rotSpeed;
+        // //     // transform.Rotate(new Vector3(0, 0,mouseY));
 
-        IEnumerator RotateAndMove(Quaternion toRotation)
-        {
-            isRotating = true;
-            float rotationTime = 0f;
+        // //     Vector3 direction = target.position - transform.position;
+        // //     float distance = direction.magnitude;
+        // //     Vector2 velocity = direction.normalized * (distance / targetRadius);
+        // //     float angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+        // //     transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        //         Debug.Log("Hello Cat");
+        //         Vector3 direction = targetPos - transform.position;
+        //         Quaternion rotation = Quaternion.LookRotation(direction);
 
-            while (!transform.rotation.Equals(toRotation))
-            {
-                
-                rotationTime += rotSpeed * Time.deltaTime;
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationTime);
-                
-                yield return null;
-            }
+        //         // Smoothly rotate towards the direction
+        //         //float rotationSpeed = 5f; // Adjust the rotation speed as needed
+        //         transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotSpeed * Time.deltaTime);
+            Vector3 direction = targetPos - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(direction);
 
-            // Rotation complete, now move towards the target position
-            isRotating = false;
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotSpeed * Time.deltaTime);
+       
         }
 
         private void MoveTo(Vector3 goalLocation)
@@ -357,59 +345,80 @@ namespace RoosaIGM601
             PathManager.RequestPath(start.position, goalLocation, OnPathFound); // Recalculate the path to the new target
         }
 
-        public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
-        {
-            if (pathSuccessful)
-            {
+        public void OnPathFound(Vector3[] newPath, bool pathSuccessful) {
+            if (pathSuccessful) {
                 path = newPath;
                 targetIndex = 0;
+                //CatRotation();
                 StopCoroutine("FollowPath");
                 StartCoroutine("FollowPath");
             }
         }
 
-        IEnumerator FollowPath()
-        {
-            if (path == null || path.Length == 0) 
-            {
-                yield break; // No path to follow, exit the coroutine
-            }
+        // IEnumerator FollowPath() {
+        // 	// Vector3 currentWaypoint = path[0];
+        // 	// while (true) {
+        // 	// 	if (transform.position == currentWaypoint) {
+        // 	// 		targetIndex ++;
+        // 	// 		if (targetIndex >= path.Length) {
+        // 	// 			yield break;
+        // 	// 		}
+        // 	// 		currentWaypoint = path[targetIndex];
+        // 	// 	}
 
-            currentWaypoint = path[0];
-            float waypointProximity = 0.1f; // Adjust as needed
-            int pathLength = path.Length;
+        // 	// 	transform.position = Vector3.MoveTowards(transform.position,currentWaypoint,speed * Time.deltaTime);
+        // 	// 	yield return null;
 
-            for (int i = 0; i < pathLength; i++) 
-            {
-                currentWaypoint = path[i];
-                currentWaypoint.y = catHeight;
+        // 	// }
 
-                while (Vector3.Distance(transform.position, currentWaypoint) > waypointProximity) 
-                {
-                    //CatRotation(currentWaypoint);
-                    transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
-                    transform.LookAt(currentWaypoint);
-                    yield return null;
-                }
-            }
+        //     Vector3 currentWaypoint = path[0];
+        //     float waypointProximity = 0.1f; // Adjust as needed
+        //     while (true) {
+        //         if (Vector3.Distance(transform.position, currentWaypoint) < waypointProximity) {
+        //             targetIndex++;
+        //             if (targetIndex >= path.Length) {
+        //                 yield break;
+        //             }
+        //             currentWaypoint = path[targetIndex];
+        //         }
+
+        //         transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+        //         yield return null;
+        //     }
+        // }
+
+        IEnumerator FollowPath() {
+        if (path == null || path.Length == 0) {
+            yield break; // No path to follow, exit the coroutine
         }
 
-        public void OnDrawGizmos()
-        {
-            if (path != null)
-            {
-                for (int i = targetIndex; i < path.Length; i++)
-                {
+        Vector3 currentWaypoint = path[0];
+        float waypointProximity = 0.1f; // Adjust as needed
+        int pathLength = path.Length;
+
+        for (int i = 0; i < pathLength; i++) {
+            currentWaypoint = path[i];
+            currentWaypoint.y = catHeight;
+
+            while (Vector3.Distance(transform.position, currentWaypoint) > waypointProximity) {
+                transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+                    transform.LookAt(currentWaypoint);
+                yield return null;
+            }
+        }
+    }
+
+        public void OnDrawGizmos() {
+            if (path != null) {
+                for (int i = targetIndex; i < path.Length; i ++) {
                     Gizmos.color = Color.cyan;
                     Gizmos.DrawCube(path[i], Vector3.one);
 
-                    if (i == targetIndex)
-                    {
+                    if (i == targetIndex) {
                         Gizmos.DrawLine(transform.position, path[i]);
                     }
-                    else
-                    {
-                        Gizmos.DrawLine(path[i - 1], path[i]);
+                    else {
+                        Gizmos.DrawLine(path[i-1],path[i]);
                     }
                 }
             }
