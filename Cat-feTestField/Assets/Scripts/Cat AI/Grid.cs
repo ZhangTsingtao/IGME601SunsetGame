d7e1ca2e3025eba03ab7a47d7120f2deef553d8f
Roosa Mozoomdar <rm7132@g.rit.edu>
2023-11-28 23:19:24 -0500

    Updated Grid with latest changes

--- Cat-feTestField/Assets/Scripts/Cat AI/Grid.cs
+++ Cat-feTestField/Assets/Scripts/Cat AI/Grid.cs
@@ -20,138 +20,203 @@ namespace RoosaIGM601
 		bool isFurniture = false;
 		public Vector3 GridWorldCenterPos;
 
-		void Awake() 
+		void Awake()
 		{
 			isFurniture = false;
-            nodeDiameter = nodeRadius*2;
-			gridSizeX = Mathf.RoundToInt(gridWorldSize.x/nodeDiameter);
-			gridSizeY = Mathf.RoundToInt(gridWorldSize.y/nodeDiameter);
+			nodeDiameter = nodeRadius * 2;
+			gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+			gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
 
-            //Tsingtao Grid Setup
-            FurnitureGridSetup();
+			//Tsingtao Grid Setup
+			FurnitureGridSetup();
 
-            CreateGrid();
+			CreateGrid();
 		}
-        private void Start()
-        {
-            PathManager.instance.grids.Add(this);
-        }
-        public void FurnitureGridSetup()
+		private void Start()
 		{
-            //Tsingtao Grid Setup
-            BoxCollider boxCollider = GetComponent<BoxCollider>();
+			PathManager.instance.grids.Add(this);
+		}
+		public void FurnitureGridSetup()
+		{
+			//Tsingtao Grid Setup
+			BoxCollider boxCollider = GetComponent<BoxCollider>();
 			if (boxCollider == null) return;
 
 			isFurniture = true;
-            gridWorldSize.x = boxCollider.bounds.extents.x * 2;
-            gridWorldSize.y = boxCollider.bounds.extents.z * 2;
-            nodeRadius = 0.1f;
-            nodeDiameter = nodeRadius * 2;
+			gridWorldSize.x = boxCollider.bounds.extents.x * 2;
+			gridWorldSize.y = boxCollider.bounds.extents.z * 2;
+			nodeRadius = 0.1f;
+			nodeDiameter = nodeRadius * 2;
 
-            gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
-            gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
-            Debug.Log(gridSizeX + ", " + gridSizeY);
+			gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+			gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+			Debug.Log(gridSizeX + ", " + gridSizeY);
 
-            unwalkableMask = LayerMask.GetMask("Unwalkable");
-            player = GameObject.Find("CatCapsule (1)").transform;
-            //Tsingtao Grid Setup End
-        }
-        public void CreateGrid() {
+			unwalkableMask = LayerMask.GetMask("Unwalkable");
+			player = GameObject.Find("CatCapsule (1)").transform;
+			//Tsingtao Grid Setup End
+		}
+		public void CreateGrid()
+		{
 			Debug.Log("Hello from Create Grid");
-			grid = new NextNode[gridSizeX,gridSizeY];
+			grid = new NextNode[gridSizeX, gridSizeY];
 
 			Vector3 worldBottomLeft = Vector3.zero;
 
-            if (isFurniture)
+			if (isFurniture)
 			{
-                BoxCollider boxCollider = GetComponent<BoxCollider>();
-                worldBottomLeft = boxCollider.bounds.center - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
-                worldBottomLeft += Vector3.up * (boxCollider.bounds.extents.y + 1 - nodeRadius);
-            }
+				BoxCollider boxCollider = GetComponent<BoxCollider>();
+				worldBottomLeft = boxCollider.bounds.center - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+				worldBottomLeft += Vector3.up * (boxCollider.bounds.extents.y + 1 - nodeRadius);
+			}
 			else
 			{
-                worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2 + (new Vector3(0, 1.5f - nodeDiameter / 2, 0));
-            }
+				worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2 + (new Vector3(0, 1.5f - nodeDiameter / 2, 0));
+			}
 
 			//worldBottomLeft += new Vector3(0, ( gridWorldSize.y / 2), 0);
 			GridWorldCenterPos = worldBottomLeft + Vector3.right * gridWorldSize.x / 2 + Vector3.forward * gridWorldSize.y / 2;
 
-            for (int x = 0; x < gridSizeX; x ++) {
-				for (int y = 0; y < gridSizeY; y ++) {
+			for (int x = 0; x < gridSizeX; x++)
+			{
+				for (int y = 0; y < gridSizeY; y++)
+				{
 					Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
-					bool openPath = !(Physics.CheckSphere(worldPoint,nodeRadius,unwalkableMask));
+					bool openPath = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
 					//Debug.Log(openPath);
-					grid[x,y] = new NextNode(openPath,worldPoint, x, y);
+					grid[x, y] = new NextNode(openPath, worldPoint, x, y);
 				}
 			}
 
 		}
 
-		public List<NextNode> GetNeighbours(NextNode node){
-		List<NextNode> neighbour = new List<NextNode>();
-		for (int x=-1; x<=1; x++){
-			for(int y=-1; y<=1; y++){
-				if(x==0 && y==0){
-					continue;
-				}
+		public List<NextNode> GetNeighbours(NextNode node)
+		{
+			List<NextNode> neighbour = new List<NextNode>();
+			for (int x = -1; x <= 1; x++)
+			{
+				for (int y = -1; y <= 1; y++)
+				{
+					if (x == 0 && y == 0)
+					{
+						continue;
+					}
 
-				int checkX = node.gridX + x;
-				int checkY = node.gridY + y;
+					int checkX = node.gridX + x;
+					int checkY = node.gridY + y;
 
-				if(checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY){
-					neighbour.Add(grid[checkX, checkY]);
+					if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+					{
+						neighbour.Add(grid[checkX, checkY]);
+					}
 				}
 			}
+
+			return neighbour;
 		}
 
-		return neighbour;
-	}
+		public List<NextNode> GetAdjacentNeighbours(NextNode node)
+        {
+            List<NextNode> AdjacentNeighbor = new List<NextNode>();
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    if (x == 0 && y == 0)
+                    {
+                        continue;
+                    }
 
-		public NextNode NodeFromWorldPoint(Vector3 worldPosition) {
-			float percentX = (worldPosition.x + gridWorldSize.x/2) / gridWorldSize.x;
-			float percentY = (worldPosition.z + gridWorldSize.y/2) / gridWorldSize.y;
+                    int checkX = node.gridX + x;
+                    int checkY = node.gridY + y;
+
+                    if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+                    {
+                        NextNode neighborNode = grid[checkX, checkY];
+
+                        // Change the condition to add only open path nodes to the list
+                        if (neighborNode.openPath) {
+                            AdjacentNeighbor.Add(neighborNode);
+                        }
+                    }
+                }
+            }
+
+            return AdjacentNeighbor;
+        }
+
+
+		public NextNode NodeFromWorldPoint(Vector3 worldPosition)
+		{
+			float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+			float percentY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
 			percentX = Mathf.Clamp01(percentX);
 			percentY = Mathf.Clamp01(percentY);
 
-			int x = Mathf.RoundToInt((gridSizeX-1) * percentX);
-			int y = Mathf.RoundToInt((gridSizeY-1) * percentY);
-			return grid[x,y];
+			int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+			int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+			return grid[x, y];
 		}
 
 		public List<NextNode> path;
-		void OnDrawGizmos() {
-			Gizmos.DrawWireCube(transform.position,new Vector3(gridWorldSize.x,1,gridWorldSize.y));
-		
-			if (grid != null) {
-				NextNode playerNode = NodeFromWorldPoint(player.position);
-				
-				foreach (NextNode n in grid) {
-					//Debug.Log(n.openPath);
-					Gizmos.color = (n.openPath)?Color.yellow:Color.red;
-					if(playerNode == n){
-						Gizmos.color = Color.cyan;
-					}
-					if(path != null){
-						if(path.Contains(n)){
-							Gizmos.color = Color.black;
-						}
-					}
-					Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter-.1f));
-				}
-			}
-		}
-        private void OnDestroy()
+		void OnDrawGizmos()
         {
-            PathManager.instance.grids.Remove(this);
+            Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+
+            if (grid != null)
+            {
+                NextNode playerNode = NodeFromWorldPoint(player.position);
+
+                foreach (NextNode n in grid)
+                {
+                    if (!n.openPath)
+                    {
+                        Gizmos.color = Color.red; // Set obstacle nodes to red
+                        Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
+
+                        // Highlight the neighbors of obstacle nodes in blue
+                        List<NextNode> neighbors = GetAdjacentNeighbours(n);
+                        foreach (NextNode neighbor in neighbors)
+                        {
+                            Gizmos.color = Color.blue;
+                            Gizmos.DrawCube(neighbor.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
+                        }
+                    }
+                    else
+                    {
+                        Gizmos.color = Color.yellow; // Set non-obstacle nodes to yellow
+
+                        if (playerNode == n)
+                        {
+                            Gizmos.color = Color.cyan;
+                        }
+
+                        if (path != null)
+                        {
+                            if (path.Contains(n))
+                            {
+                                Gizmos.color = Color.black;
+                            }
+                        }
+
+                        Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
+                    }
+                }
+            }
         }
-    }
+
+		private void OnDestroy()
+		{
+			PathManager.instance.grids.Remove(this);
+		}
+	}
 }
 
 // public class Node1 {
-	
+
 // 	public bool walkable;
 // 	public Vector3 worldPosition;
-	
+
 // 	public Node(bool _walkable, Vector3 _worldPos) {
 // 		walkable = _walkable;
 // 		worldPosition = _worldPos;
